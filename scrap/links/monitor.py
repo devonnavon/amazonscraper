b48e721f53a00d4410974d811c24e22729a1fd44
Devon Navon <devonnavon@MacBook-Pro.local>
2019-12-07 20:46:19 -0600

    searchpage class done, organized a bit

---
+++ scrap/links/monitor.py
@@ -0,0 +1,44 @@
+
+
+reddit = [
+r'https://www.reddit.com/r/buildapc/comments/d6cl27/best_no_compromises_1440p_144hz_monitor/',
+r'https://www.reddit.com/r/buildapc/comments/deev4n/help_me_pick_a_24_1080p_144hz_monitor_for_my/',
+r'https://www.reddit.com/r/buildapc/comments/5htrac/detailed_guide_to_buying_a_monitor'
+]
+
+guides = [
+r'https://thewirecutter.com/reviews/best-4k-monitors/',
+r'https://www.imore.com/best-4k-monitors-mac',
+r'https://www.tomshardware.com/reviews/best-gaming-monitors,4533.html',
+r'https://www.tftcentral.co.uk/reviews.htm',
+r'https://www.lifewire.com/best-monitors-for-macbook-pro-4169493',
+r'https://www.pcmag.com/roundup/295820/the-best-computer-monitors',
+r'https://www.techradar.com/news/computing-components/peripherals/best-monitor-9-reviewed-and-rated-1058662',
+r'https://www.rtings.com/monitor/reviews/best/monitors',
+]
+
+products = [
+r'https://www.amazon.com/dp/B01MY142C0/?tag=aboutcom02lifewire-20&ascsubtag=4169493|n0ddb881c3d56403f8ae18010024cb28d21',
+r'https://www.samsung.com/us/computing/monitors/uhd-and-wqhd/samsung-uhd-28-monitor-with-high-glossy-black-finish-lu28e590ds-za/?CID=afl-ecomm-cjn-cha-092118-52057&cjevent=e8e27a05fe8311e9815a00ce0a1c0e0b',
+r'https://store.hp.com/us/en/pdp/hp-z27-27-inch-4k-uhd-display?source=aw&subacctid=256015&subacctname=Wirecutter+Inc.&adcampaigngroup=91539&awc=7168_1572817846_894fd3a9a8f5a8b7438adf237a3a5fed&jumpid=af_gen_nc_ns&utm_medium=af&utm_source=aw&utm_campaign=Wirecutter+Inc.',
+]
+
+wirecutter = r'https://thewirecutter.com/reviews/best-4k-monitors/'
+
+wirecutter_products_amazon = [
+r'https://www.amazon.com/HP-Business-Z27-LED-Monitor/dp/B07CKXCBWB/ref=pd_di_sccai_8?_encoding=UTF8&pd_rd_i=B07CKXCBWB&pd_rd_r=46f03b62-2fca-4b9b-a338-7716717277d3&pd_rd_w=z44Lk&pd_rd_wg=RFwUB&pf_rd_p=91969358-e80f-4d1e-9cd9-073577930a16&pf_rd_r=D39F7GBFK3HM9B8MN9Y3&psc=1&refRID=D39F7GBFK3HM9B8MN9Y3',
+r'https://www.amazon.com/dp/B07DPTJ2ZG/?tag=thewire06-20&linkCode=xm2&ascsubtag=AwEAAAAAAAAAAYYB',
+r'https://www.amazon.com/Dell-Ultrasharp-U2718Q-27-Inch-Monitor/dp/B073VYVX5S/ref=sr_1_2?keywords=Dell+UltraSharp+U2718Q&qid=1573498879&s=electronics&sr=1-2',
+r'https://www.amazon.com/dp/B07HDBD9CM/?tag=thewire06-20&linkCode=xm2&ascsubtag=AwEAAAAAAAAAAYYD',
+r'https://www.amazon.com/dp/B00PC9HFNY/?tag=thewire06-20&linkCode=xm2&ascsubtag=AwEAAAAAAAAAAYYE',
+]
+
+wirecutter_products_other = [
+r'https://store.hp.com/us/en/pdp/hp-z27-27-inch-4k-uhd-display?source=aw&subacctid=256015&subacctname=Wirecutter+Inc.&adcampaigngroup=91539&awc=7168_1573500579_6f4c7c6656ec4df48eaa6b585682a109&jumpid=af_gen_nc_ns&utm_medium=af&utm_source=aw&utm_campaign=Wirecutter+Inc.',
+r'https://www.bhphotovideo.com/c/product/1464835-REG/lenovo_61cbgar1us_27_thinkvision_p27u_10_wide.html/DFF/d10-v21-t1-x949679/SID/45704X1167592X4f7b923b891bb455023489d07279f001',
+r'https://www.bestbuy.com/site/dell-ultrasharp-u2718q-27-ips-led-4k-uhd-monitor-black/6079912.p?skuId=6079912&irclickid=TL-wKe0m5xyORyU0RHQK3XRkUknw6zVJMXdk1E0&irgwc=1&ref=198&loc=TL-wKe0m5xyORyU0RHQK3XRkUknw6zVJMXdk1E0&acampID=614286',
+r'https://www.dell.com/en-us/work/shop/accessories/apd/210-amlm?prg=1&VEN1=13003219-5513721-AwEAAAAAAAAAAYYC&cjevent=2ee61f4304ba11ea821e000b0a1c0e12&dgc=BF&DGSeg=BSD&cid=198376&lid=45675&acd=12309198376456750&VEN3=110105164266465092',
+r'https://www.dell.com/en-us/shop/dell-ultrasharp-32-4k-usb-c-monitor-u3219q/apd/210-aqzz/monitors-monitor-accessories?prg=1&VEN1=12839518-5513721-AwEAAAAAAAAAAYYD&AID=5513721&cjevent=43a2cc8a04ba11ea83b6000b0a1c0e0d&dgc=CJ&DGSeg=DHS&cid=198375&lid=45846&acd=12309198375458460&VEN3=110105164266465092',
+r'https://www.dell.com/en-us/work/shop/dell-24-monitor-p2415q/apd/210-agnk/monitors-monitor-accessories?prg=1&VEN1=13003219-5513721-AwEAAAAAAAAAAYYE&cjevent=4c4a5c7304ba11ea8335000b0a1c0e11&dgc=BF&DGSeg=BSD&cid=198376&lid=45675&acd=12309198376456750&VEN3=110105164266465092',
+r'https://www.walmart.com/ip/Dell-P2415Q-LED-monitor-4K-23-8-with-3-Years-Advanced-Exchange-Warranty/43392279?irgwc=1&sourceid=imp_3SJTgG0m%3AxyOWEvwUx0Mo34GUknw6U08PSkA1k0&veh=aff&wmlspartner=imp_197432&clickid=3SJTgG0m%3AxyOWEvwUx0Mo34GUknw6U08PSkA1k0',
+]
---
+++ search/search_page.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import requests
+import sys
+from bs4 import BeautifulSoup as bs
+
+
+class SearchPage(object):
+    def __init__(self, query, page_number=None):
+        self.query = query
+        self.page_number = page_number
+        self.response = self.get_response()
+        self.soup = self.get_soup()
+        self.results = self.get_result_list()
+
+    @property
+    def search_url(self):
+        search_url = 'https://www.amazon.com/s?k='
+        search_text = self.query.replace(' ','+')
+        if self.page_number == None:
+            pn = ''
+        else:
+            pn = str(self.page_number)
+        url = search_url + search_text + '&page='+pn
+        return url
+
+    def get_response(self):
+        headers = {
+            "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
+            "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"
+            }
+        r = requests.get(self.search_url, headers=headers)
+        return r
+
+    def get_soup(self):
+        soup = bs(self.response.content, features='lxml')
+        return soup
+
+    def get_result_list(self):
+        """
+        gets portion of page that is just results
+        """
+        return self.soup.find('div', attrs={'class':'s-result-list s-search-results sg-row'})
+
+    #non init or property functions
+    def get_max_pages(self):
+        """
+        gets max pages displayed on page (should only be used on first page)
+        """
+        page_counter = self.soup.find('div', attrs={'class':'a-section a-spacing-none a-padding-base'})
+        num_pages = page_counter.findAll('li', attrs={'class':'a-disabled'})[-1].text
+        return int(num_pages)
+
+    def get_product_divs(self):
+        """
+        gets list of product within result list
+        """
+        product_divs = self.results.select('div[data-asin]')
+        #filter out divs that don't have amazon_id
+        product_divs = list(filter(lambda x: len(x.attrs['data-asin']) > 0, product_divs))
+        return product_divs
+
+    #@staticmethod
+    def get_product_info(self,div):
+        """
+        gets info for individual product divs
+        """
+        #self dummy
+        amazon_id = div.attrs['data-asin']
+        name = div.find('span', attrs={'class':'a-size-base-plus a-color-base a-text-normal'}).text
+        price = div.find('span', attrs={'class':'a-offscreen'})
+        if price is not None:
+            price = price.text
+        else:
+            pass
+        sponsored = div.find('span', attrs={'class':'a-size-base a-color-secondary'})
+        if sponsored is not None:
+            is_sponsored = True
+        else:
+            is_sponsored = False
+        return {
+            'amazon_id' : amazon_id,
+            'name' : name,
+            'price' : price,
+            'is_sponsored': is_sponsored
+        }
+
+    def get_products(self):
+        """
+        uses get_product_divs() and get_product_info()
+        """
+        products = []
+        for div in self.get_product_divs():
+            products.append(self.get_product_info(div))
+        return products

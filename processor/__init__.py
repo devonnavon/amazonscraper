5bb927b1c89dddd9b468bcdb961b84dbb727dbbe
Devon Navon <devonnavon@MacBook-Pro.local>
2019-12-29 01:39:37 -0800

    it works 537537$

--- processor/__init__.py
+++ processor/__init__.py
@@ -6,11 +6,11 @@ from bs4 import BeautifulSoup as bs
 
 
 class Page(object):
-    def __init__(self, url):
-        self.url = url
+    def __init__(self, base_url):
+        self.base_url = base_url
 
-    def set_args(self, arg):
-        url.format(arg)
+    def set_args(self, page_num, *args):
+        self.url = self.base_url.format(page_num=page_num, *args)
 
     def get_response(self):
         headers = {
@@ -18,47 +18,101 @@ class Page(object):
             "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
             "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"
             }
-        r = requests.get(self.search_url, headers=headers)
-        return r
+        self.response = requests.get(self.url, headers=headers)
 
     def get_soup(self):
-        soup = bs(self.response.content, features='lxml')
-        return soup
+        self.get_response()
+        self.soup = bs(self.response.content, features='lxml')
 
-    def get_info():
-        pass
+    def get_info():#will want to rewrite this every time
+        self.get_soup()
+        return None
 
+class Search(Page):
+    def __init__(self):
+        self.base_url = 'https://www.amazon.com/s?k={}&page={page_num}'
+
+    def get_info(self):
+        self.get_soup() #runs self.soup
+        results = self.soup.find('div', attrs={'class':'s-result-list s-search-results sg-row'})
+        product_divs = results.select('div[data-asin]')
+        product_divs = list(filter(lambda x: len(x.attrs['data-asin']) > 0, product_divs))
+        products = []
+        for div in product_divs:
+            amazon_id = div.attrs['data-asin']
+
+            #must be a more elegant way to do this
+            name = div.find('span', attrs={'class':'a-size-base-plus a-color-base a-text-normal'})
+            if name is not None:
+                name = name.text
+            else:
+                name = div.find('span', attrs={'class':'a-size-medium a-color-base a-text-normal'})
+                if name is not None:
+                    name = name.text
+                else:
+                    pass
+
+            price = div.find('span', attrs={'class':'a-offscreen'})
+            if price is not None:
+                price = price.text
+            else:
+                pass
+
+            sponsored = div.find('span', attrs={'class':'a-size-base a-color-secondary'})
+            if sponsored is not None:
+                is_sponsored = True
+            else:
+                is_sponsored = False
+
+            products.append({
+                'amazon_id' : amazon_id,
+                'name' : name,
+                'price' : price,
+                'is_sponsored': is_sponsored
+            })
+        return products
 
 class Runner(object):
-    def __init__(self, runs):
-        self.runs = runs
+    def __init__(self, page):
         self.q = multiprocessing.Queue()
         self.t = {} #empty dict of threads or processes
-        self.page = page
-        self.results = [] #empty till run
+        self.page = page #page object
 
-    def put_page(self, arg):
-        self.page.set_args(args)
+    def put_page(self, page_num, *args):
+        self.page.set_args(page_num, *args)
         self.q.put(self.page.get_info())
 
-    def run(self, run_info=False):
-        results = []
-        startTime = time.time()
+    def run(self, pages, *args):
+        '''
+        pages int for number of pages to get
+        args = page parameters
+        '''
+        self.qcount = 0 #empty till run
+        self.results = [] #empty till run
+        self.start_time = time.time()
         qcount = 0
 
-        for arg in self.runs:
-            self.p[arg] = threading.Thread(target=self.put_page, args=arg)
-            p[page_num].start()
+        for page_num in range(1, pages+1):
+            #temp = (page_num,)+args
+            self.t[page_num] = threading.Thread(target=self.put_page, args=(page_num,)+args)
+            self.t[page_num].start()
 
         for thread in self.t:
-            thread.join()
+            self.t[thread].join()
 
-        while not q.empty():
-            queue_top = q.get()
+        while not self.q.empty():
+            queue_top = self.q.get()
             self.qcount += 1
             self.results+=queue_top
 
-        self.run_time = time.time()-startTime
+        self.run_time = time.time()
+        self.time_elapsed = self.run_time-self.start_time
+        self.run_info = {
+                        'queue_count': self.qcount,
+                        'time_elapsed': self.time_elapsed,
+                        'run_time': self.run_time
+                        }
+        return self.run_info
 
     def get_results(self):
         return self.results
